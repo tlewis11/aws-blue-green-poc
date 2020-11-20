c6cad7ca3b71d5fd5498371bf499e53cde4ebdf8
tlewis11 <tim.lewis@stelligent.com>
2020-11-19 23:27:15 -0600

    test agains elb dns

--- test_app.py
+++ test_app.py
@@ -1,14 +1,20 @@
+import boto3
 import requests
+import time
 
 if __name__ == '__main__':
-
-  #discover application ip address
-  instance_ip = '3.137.182.152'
+  outputs = boto3.client('cloudformation', region_name='us-east-2').describe_stacks(StackName='demo-loadbalancer')['Stacks'][0]['Outputs']
+  elb_dns = [output['OutputValue'] for output in outputs if output['OutputKey'] == 'LoadBalancerDns'][0]
   #run basic tests against ip address
   # should return 200
-  uri = f'http://{instance_ip}'
+  time.sleep(40)
+  uri = f'http://{elb_dns}'
   r = requests.get(uri)
   code = r.status_code
   expected_code = 200
+  print('testing elb dns returns 200...')
   assert code is expected_code , f'Expected {expected_code} status code. Instance returned {code}'
-  assert 'My Instance!' in r.text, 'Web page did not contain the correct contents.'
+  print('good')
+  print('testing contents of webpage...')
+  assert 'Hello from' in r.text, 'Web page did not contain the correct contents.'
+  print('good')
